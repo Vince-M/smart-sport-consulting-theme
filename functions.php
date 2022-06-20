597a78f2b8b0527fd6480de08104d15b2a3730db
Vince Mendella <vmendella@gmail.com>
2022-06-20 18:29:16 -0500

    update styles throughout site

--- functions.php
+++ functions.php
@@ -27,6 +27,10 @@ function register_theme_menus() {
 }
 add_action( 'after_setup_theme', 'register_theme_menus' );
 
+remove_filter( 'render_block', 'wp_render_layout_support_flag', 10, 2 );
+
+remove_filter( 'render_block', 'gutenberg_render_layout_support_flag', 10, 2 );
+
 
 
 ?>
