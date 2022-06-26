9f8cbc619c01ffea94b10f6e32ebf89bb0e14ab9
Vince Mendella <vmendella@gmail.com>
2022-06-26 10:21:02 -0500

    add alt and title tags for ACF fields on blog

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
 function smartsport_files() {
-  wp_enqueue_script('smartsport_js', get_template_directory_uri() . '/dist/script.js', NULL, '', true );
+  wp_enqueue_script('smartsport_js', get_stylesheet_directory_uri() . '/dist/script.js', NULL, '', true );
   wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;1,400&family=Roboto:wght@400;700;900&display=swap');
   wp_enqueue_style('smartsport_main_styles', get_theme_file_uri('main.css'));
   }
--- single.php
+++ single.php
@@ -1,5 +1,17 @@
 <?php get_header(); ?>
 
+<?php 
+  $image = get_field( 'profile-hero' );
+  $size = $image['sizes']['large'];
+  $alt = $image['alt'];
+  $title = $image['title'];
+
+  $icon = get_field( 'profile-icon' );
+  $iconSize = $icon['sizes']['large'];
+  $iconAlt = $icon['alt'];
+  $iconTitle = $icon['title'];
+?>
+
   <!-- HERO
   =================================================== -->
   <section class="hero feature-image" role="banner" data-type="background" data-speed="2">
@@ -25,27 +37,26 @@
         <div class="blog__content--single">
             
           <aside class="blog__sidebar">
-            <img class="blog__sidebar--profileImage" src="<?php the_field( 'profile-image' ); ?>" alt="">
+            <img src="<?php echo $iconSize; ?>" class="blog__sidebar--profileImage" alt="<?php echo $iconAlt; ?>" title="<?php echo $iconTitle; ?>" >
           </aside>
 
           <section class="blog__main">
             
-
             <header class="blog__header">
             
-            <?php if ( 'post' == get_post_type() ) : ?>
-              <?php the_title(); ?>
-            <div class="post-details">
-            <i class="fa fa-clock-o"></i> <time><?php echo get_the_date(); ?></time>
-            
-            <i ></i> <?php the_category(', ') ?>
+                <?php if ( 'post' == get_post_type() ) : ?>
+                  <?php the_title(); ?>
 
-            </div><!-- post-details -->
+                <div class="post-details">
+                  <time><?php echo get_the_date(); ?></time>
 
-            <?php endif; ?>
-        </header><!-- .entry-header -->
+                  <?php the_category(', ') ?>
+                </div><!-- post-details -->
 
-        <img class="blog__hero--image" src="<?php the_field( 'profile-hero' ); ?>" alt="">
+                <?php endif; ?>
+            </header><!-- .entry-header -->
+
+            <img src="<?php echo $size; ?>" class="blog__hero--image" alt="<?php echo $alt; ?>" title="<?php echo $title; ?>" >
 
             <?php the_content(); ?>
           </section>
