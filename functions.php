605da9e12b9a0c28e139b66d99a190d04b6acf42
Vince Mendella <vmendella@gmail.com>
2022-08-25 09:31:29 -0500

    remove closing of one php block and the top of the second php block to make one php block

--- functions.php
+++ functions.php
@@ -1,26 +1,30 @@
 <?php
 
 function smartsport_files() {
-  wp_enqueue_script('smartsport_js', get_stylesheet_directory_uri() . '/dist/script.js', NULL, '', false );
   wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;1,400&family=Roboto:wght@400;700;900&display=swap');
   wp_enqueue_style('smartsport_main_styles', get_theme_file_uri('main.css'));
+  wp_enqueue_style( 'smartsport_styles', get_theme_file_uri('style.css'));
+  wp_enqueue_script( 'smartsport_js', get_stylesheet_directory_uri() . '/dist/script.js', NULL, '1.0', false);
   }
-
 add_action('wp_enqueue_scripts', 'smartsport_files');
 
-add_theme_support( 'menus' );
-add_theme_support( 'post-thumbnails' );
-add_theme_support( 'title-tag' );
 
-add_action('after_setup_theme', 'smartsport_files');
+function smartsport_features() {
+  add_theme_support( "wp-block-styles" );
+  add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption', 'style', 'script' ) );
+  add_theme_support( 'automatic-feed-links' );
+  add_theme_support( 'post-thumbnails' );
+  add_theme_support( 'title-tag' );
+}
+
 
 
 // Theme support
 function register_theme_menus() {
   register_nav_menus(
     array(
-      'header-menu'  =>  __( 'Header Menu' ),
-      'footer-menu'  =>  __( 'Footer Menu' )
+      'header-menu'  =>  'Header Menu',
+      'footer-menu'  =>  'Footer Menu'
     )
   );
 
@@ -33,10 +37,6 @@ remove_filter( 'render_block', 'gutenberg_render_layout_support_flag', 10, 2 );
 
 
 
-?>
-
-<?php
-
 function defer_parsing_of_js( $url ) {
     if ( is_user_logged_in() ) return $url; //don't break WP Admin
     if ( FALSE === strpos( $url, '.js' ) ) return $url;
@@ -45,4 +45,7 @@ function defer_parsing_of_js( $url ) {
 }
 add_filter( 'script_loader_tag', 'defer_parsing_of_js', 10 );
 
-?>
+function add_comment_to_body_open() {
+  printf( "<!-- THIS SHOULD RENDER AFTER THE BODY TAG OPENS --" );
+}
+add_action( 'wp_body_open', 'add_comment_to_body_open' );
